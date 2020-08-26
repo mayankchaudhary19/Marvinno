a37111d20789a0147508f1d70a6c665de403c01d
Mayank Chaudhary <mayank1991999@gmail.com>
2020-08-26 13:08:57 +0530

    removed errors from splash

--- app/src/main/java/com/example/marvinno/SplashActivity.java
+++ app/src/main/java/com/example/marvinno/SplashActivity.java
@@ -14,23 +14,24 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE); //no title
+        setContentView(R.layout.activity_splash);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE); //no title
         getSupportActionBar().hide(); // hide the title bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
-        setContentView(R.layout.activity_splash);
+
 
         handler=new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                Intent intent=new Intent(SplashActivity.this,IntroActivity.class);
                 startActivity(intent);
                 finish();
             }
         },3000);
 
-        Intent intent = new Intent(this, SplashActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, SplashActivity.class);
+//        startActivity(intent);
 
     }
 }
