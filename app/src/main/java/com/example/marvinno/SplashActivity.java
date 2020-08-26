ec910b35419fa7ce30d7095e2958fd7b3ec91e4e
PRIYA RAWAT <priyarawat0810>
2020-08-26 12:17:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/marvinno/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.marvinno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //no title
+        getSupportActionBar().hide(); // hide the title bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_splash);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
